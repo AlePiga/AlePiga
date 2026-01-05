f59432a6fe84b588e6975f13ed3d19ad9b7ffaee
Alessandro Pigaiani <134812981+AlePiga@users.noreply.github.com>
2026-01-05 02:37:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function updateDateTime() {
-	const now = new Date();
-	const year = now.getFullYear();
-	const month = now.getMonth() + 1;
-	const day = now.getDate();
-	const hours = String(now.getHours()).padStart(2, "0");
-	const minutes = String(now.getMinutes()).padStart(2, "0");
-	const seconds = String(now.getSeconds()).padStart(2, "0");
-	const milliseconds = String(Math.floor(now.getMilliseconds() / 10)).padStart(2, "0");
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const milliseconds = String(Math.floor(now.getMilliseconds() / 10)).padStart(2, "0");
 
-	const formattedDateTime = `${month} ${day} ${year} + "\n" + ${hours}:${minutes}:${seconds}:${milliseconds}`;
-	document.getElementById("datetime").textContent = formattedDateTime;
+  const formattedDateTime = `${month} ${day} ${year}\n${hours}:${minutes}:${seconds}:${milliseconds}`;
+  document.getElementById("datetime").textContent = formattedDateTime;
 }
 
 setInterval(updateDateTime, 1);
